ae3abab546f5e5ff1307e1936fc36bc76dac1999
Yahya <yhmoha66@gmail.com>
2023-03-16 11:25:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solution in this file!
+let headQuatersLocation = 42;
+
+function distanceFromHqInBlocks(pickupLocation){
+    let numberOfBlocks = 0;
+    if(pickupLocation > headQuatersLocation){
+        numberOfBlocks = pickupLocation - headQuatersLocation;
+    }else if(headQuatersLocation > pickupLocation){
+        numberOfBlocks = headQuatersLocation - pickupLocation;
+    }
+    return numberOfBlocks;
+
+}
+
+function distanceFromHqInFeet(pickupLocation){
+    let distanceInFeet = distanceFromHqInBlocks(pickupLocation) * 264;
+    return distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let distanceTravelled = 0;
+    if(start > destination){
+        distanceTravelled = (start - destination) * 264;
+    }
+    else if(destination > start){
+        distanceTravelled = (destination - start) * 264;
+    }
+    return distanceTravelled;
+}
+
+function calculatesFarePrice(start, destination){
+    let fairPrice = 0;
+    let distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400 ){
+        fairPrice = 0;
+    }
+    else if(distance > 400 && distance <= 2000){
+        fairPrice = ((distance * 2) - (400 * 2)) / 100;
+    }
+    else if(distance > 2000 && distance <= 2500){
+        fairPrice = 25;
+    }else{
+        return 'cannot travel that far';
+    }
+
+    return fairPrice;
+}
+
